3a0925589d83b2b3c65f35a782a056e394f2876e
imran <smemrannit@gmail.com>
2024-10-03 20:53:15 +0600

    loaded vidoes

--- script/video.js
+++ script/video.js
@@ -1,8 +1,42 @@
+// create load categorys
 const loadaCategores = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
         .then(res => res.json())
         .then(data => displayCategores(data.categories))
         .catch(err => console.log(err))
+}
+
+// creat load videos
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+        .catch(err => console.log(err))
+}
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById('videos');
+    // console.log(videos);
+    videos.forEach((video) => {
+        let nav = document.createElement("div");
+        nav.classList = "card card-compact";
+        nav.innerHTML = `
+        <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        `
+        videoContainer.append(nav);
+    });
+
 }
 
 const displayCategores = (data) => {
@@ -23,3 +57,4 @@ const displayCategores = (data) => {
 }
 
 loadaCategores();
+loadVideos();
