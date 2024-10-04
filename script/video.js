1ba1d3cb1e01e9f7debb53e73dbfa59aa54c3272
imran <smemrannit@gmail.com>
2024-10-04 12:04:51 +0600

    video and title, verify icon added

--- script/video.js
+++ script/video.js
@@ -14,23 +14,36 @@ const loadVideos = () => {
         .catch(err => console.log(err))
 }
 
+// const carddemo = {
+//     {category_id: '1003', video_id: 'aaaf', thumbnail: 'https://i.ibb.co/5LRQkKF/stick-and-stones.jpg', title: 'Sticks & Stones',{profile_picture: 'https://i.ibb.co/rdTZrCM/dev.jpg', profile_name: 'Dave Chappelle', verified: true}
+// }
+
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById('videos');
     // console.log(videos);
     videos.forEach((video) => {
+        console.log(video);
         let nav = document.createElement("div");
         nav.classList = "card card-compact";
         nav.innerHTML = `
-        <figure>
+        <figure class="h-[200px relative">
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src=${video.thumbnail}
+      class="h-full w-full object-cover"
       alt="Shoes" />
+      <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${video.others.posted_date}</span>
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2 flex gap-2">
+    <div>
+    <img class="w-10 h-10 rounded-full" src=${video.authors[0].profile_picture} />
+    </div>
+    <div>
+    <h2 class="font-bold">${video.title}</h2>
+    <div class="flex items-center gap-3">
+    <p>${video.authors[0].profile_name}</p>
+    ${video.authors[0].verified == true? '<img class="w-5" src="https://cdn-icons-png.flaticon.com/128/7641/7641727.png"/>' : ""}
+    </div>
+    <p></p>
     </div>
   </div>
         `
